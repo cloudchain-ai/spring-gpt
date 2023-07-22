ec9255555c0e6863ac80424af5dc49b768e27290
Sushil Prasad <cse.sushil@gmail.com>
2023-07-22 20:24:23 +0530

    spring-boot-version-upgrade

---
+++ src/main/java/com/smallintro/springgpt/SpringGptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smallintro.springgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGptServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smallintro/springgpt/SpringGptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallintro.springgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
